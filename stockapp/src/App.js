2960f5fa7c722ffb2bfe9dc84054dd0b7aa65226
NF09830453 <fyre.lord31@gmail.com>
2023-11-07 00:15:40 -0500

    frontPage

--- stockapp/src/App.js
+++ stockapp/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and sYOLO to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="/logoStockm.png"></img>
+        <button onClick = {startRound} class= "round">Start Round</button>
+        <button onClick = {configurePlayer} class= "round">Configure Player</button>
+        <button onClick = {configureAI} class= "round">Configure AI</button>
+        <button onClick = {configureGameMode} class= "round">Game Mode</button>
       </header>
     </div>
   );
 }
 
+function startRound() {
+  // Handle logic for starting a round
+}
+
+function configurePlayer() {
+  // Handle logic for configuring players
+}
+
+function configureAI() {
+  // Handle logic for configuring AI
+}
+
+function configureGameMode() {
+  // Handle logic for configuring the game mode
+}
 export default App;
